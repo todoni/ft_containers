cd7df3e5dd6772325ffc305920df4288c27398f9
sohan <sohan@student.42seoul.kr>
2022-09-20 18:37:04 +0900

    [feat] map member functions declaration

--- ft_map.hpp
+++ ft_map.hpp
@@ -4,6 +4,7 @@
 # include <memory>
 # include <functional>
 # include <utility> //TODO: ft::pair 구현 후 바꿔야함
+# include "ft_iterator.hpp"
 
 namespace ft
 {
@@ -31,6 +32,9 @@ public:
 	{
 
 	};
+	typedef	ft::reverse_iterator<iterator>							reverse_iterator;
+	typedef typename ft::iterator_traits<iterator>::difference_type	difference_type;
+	typedef typename allocator_type::size_type				size_type;
 
 	class	value_compare : public std::binary_function<value_type, value_type, bool> {
 		friend class map<Key, T, Compare>;
@@ -41,6 +45,53 @@ public:
       	bool operator()(const value_type& x, const value_type& y) const { return comp(x.first, y.first); }
 	};
 
+
+	explicit map(const key_compare& comp = key_compare(),
+					const allocator_type& alloc = allocator_type());
+	template <class InputIterator>
+	map(InputIterator first, InputIterator last,
+       			const key_compare& comp = key_compare(),
+       			const allocator_type& alloc = allocator_type());
+	map(const map& x);
+	~map();
+
+	iterator begin();
+	//const_iterator begin() const;
+	iterator end();
+	//const_iterator end() const;
+	reverse_iterator rbegin();
+	//const_reverse_iterator rbegin() const;
+	reverse_iterator rend();
+	//const_reverse_iterator rend() const;
+	bool empty() const;
+	size_type size() const;
+	size_type max_size() const;
+
+	mapped_type& operator[] (const key_type& k);
+	
+	typedef std::pair<iterator, bool> pair_iterator_bool;
+	pair_iterator_bool insert(const value_type& val);
+	iterator insert(iterator position, const value_type& val);
+	template <class InputIterator>
+  	void insert (InputIterator first, InputIterator last);
+	void erase(iterator position);
+	size_type erase(const key_type& k);
+	void erase(iterator first, iterator last);
+	void swap(map& x);
+	void clear();
+	key_compare key_comp() const;
+	value_compare value_comp() const;
+
+	iterator find(const key_type& k);
+	//const_iterator find(const key_type& k) const;
+	size_type count(const key_type& k) const;
+	iterator lower_bound(const key_type& k);
+	//const_iterator lower_bound (const key_type& k) const;
+	iterator upper_bound(const key_type& k);
+	//const_iterator upper_bound (const key_type& k) const;
+	//std::pair<const_iterator,const_iterator> equal_range(const key_type& k) const;
+	pair_iterator_bool	equal_range(const key_type& k);
+	allocator_type	get_allocator() const;
 };
 
 }
