d45f0f0ee0263b421bb959a6ae9ff2d0b15e6d89
sohan <sohan@student.42seoul.kr>
2022-09-20 13:48:37 +0900

    [add] tree file

---
+++ tree.h
@@ -0,0 +1,16 @@
+#ifndef TREE_H
+# define TREE_H
+
+# include <memory>
+# include <functional>
+# include <utility> //TODO: ft_utility.hpp
+
+template<class Key, class T, class Compare = std::less<Key>,
+           class Allocator = std::allocator<std::pair<const Key, T> > >
+class	avl_tree
+{
+
+};
+
+
+#endif
