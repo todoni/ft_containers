8c485185ac44e41351913d973a9975a28c269ec8
sohan <sohan@student.42seoul.kr>
2023-02-21 16:51:28 +0900

    Fix: generic expression for typedef

--- ft_vector.hpp
+++ ft_vector.hpp
@@ -22,14 +22,14 @@ class vector
 public:
 	typedef T value_type;
 	typedef Allocator allocator_type;
-	typedef T& reference;
-	typedef const T& const_reference;
-	typedef T* pointer;
-	typedef const T* const_pointer;
+	typedef typename allocator_type::reference reference;
+	typedef typename allocator_type::const_reference const_reference;
+	typedef typename allocator_type::pointer pointer;
+	typedef typename allocator_type::const_pointer const_pointer;
 	typedef pointer iterator;
 	typedef const_pointer const_iterator;
-	typedef std::size_t size_type;
-	typedef long difference_type;
+	typedef size_t size_type;
+	typedef typename ft::iterator_traits<iterator>::difference_type difference_type;
 	typedef ft::reverse_iterator<iterator>	reverse_iterator;
 	typedef ft::reverse_iterator<const_iterator>	const_reverse_iterator;
 
